97ac613084a8a82961e82bf318b217fa6ee17398
Sara Katerin Acuña Murcia <saramurcia06@outlook.com>
2021-04-28 22:26:25 -0500

    asset

--- main/dashboard.php
+++ main/dashboard.php
@@ -333,9 +333,70 @@
         <!-- ============================================================== -->
         <!-- Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
-        <?php 
-         include ('Include/sidebar_admin.php');
-        ?>
+        <aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="nav-small-cap">--- Administrador</li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="icon-Car-Wheel"></i><span class="hide-menu">Gestiòn Citas <span </span></span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="form-citas.php">Gestion de citas </a></li>
+                                <li><a href="table-citas.php">Listado de citas </a></li>
+                                
+
+                             
+                            </ul>
+                        </li>
+                        
+                       
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="icon-Receipt-4"></i><span class="hide-menu">Pacientes</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                
+                                <li><a href="form-layout.php">Registrar</a></li>
+                                <li><a href="table-footable.php">Listado</a></li>
+                               
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="icon-Receipt-4"></i><span class="hide-menu">Personal</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                
+                                <li><a href="form-personal.php">Registrar</a></li>
+                                <li><a href="table-personal.php">Listado</a></li>
+                               
+                            </ul>
+                        </li>
+                       
+
+                                </li>
+                            </ul>
+                        </li>
+                        
+                        </li>
+                        
+                       
+                        
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="#">item 1.1</a></li>
+                                <li><a href="#">item 1.2</a></li>
+                                <li> <a class="has-arrow" href="#" aria-expanded="false">Menu 1.3</a>
+                                    <ul aria-expanded="false" class="collapse">
+                                        <li><a href="#">item 1.3.1</a></li>
+                                        <li><a href="#">item 1.3.2</a></li>
+                                        <li><a href="#">item 1.3.3</a></li>
+                                        <li><a href="#">item 1.3.4</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">item 1.4</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
         <!-- ============================================================== -->
         <!-- End Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
